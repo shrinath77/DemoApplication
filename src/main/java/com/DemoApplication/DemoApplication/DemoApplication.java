30222f00fc7a59900b68f13337aa5951d5feeaa4
shrinath.kadam <kadamshrinath7777@gmail.com>
2025-08-21 12:18:11 +0530

    Update DemoApplication.java
    Added Logger

--- src/main/java/com/DemoApplication/DemoApplication/DemoApplication.java
+++ src/main/java/com/DemoApplication/DemoApplication/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.DemoApplication.DemoApplication;
 
+import com.DemoApplication.DemoApplication.controller.UserController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(UserController.class);
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+        logger.info("Application starting");
 	}
 
 }
